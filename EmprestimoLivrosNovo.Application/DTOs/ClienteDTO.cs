67d6d0946033e77c3bc581807a99c913d7facce1
Vinicius <56903834+mrtrycatch@users.noreply.github.com>
2024-01-20 00:18:31 -0300

    Ajuste formato dos erros e mensagem de erro personalizada no data annotation de cliente.

--- EmprestimoLivrosNovo.Application/DTOs/ClienteDTO.cs
+++ EmprestimoLivrosNovo.Application/DTOs/ClienteDTO.cs
@@ -12,28 +12,28 @@ namespace EmprestimoLivrosNovo.Application.DTOs
     {
         [IgnoreDataMember]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O CPF é obrigatório")]
         [StringLength(11,MinimumLength = 11, ErrorMessage = "O CPF deve ter 11 caracteres")]
         public string CliCPF { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Nome é obrigatório")]
         [StringLength(200, ErrorMessage = "O Nome deve ter 200 caracteres")]
         public string CliNome { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Endereço é obrigatório")]
         [StringLength(50, ErrorMessage = "O Endereço deve ter 50 caracteres")]
         public string CliEndereco { get; set; }
-        [Required]
+        [Required(ErrorMessage = "A Cidade é obrigatória")]
         [StringLength(50, ErrorMessage = "A Cidade deve ter 50 caracteres")]
         public string CliCidade { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Bairro é obrigatório")]
         [StringLength(50, ErrorMessage = "O Bairro deve ter 50 caracteres")]
         public string CliBairro { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Número é obrigatório")]
         [StringLength(50, ErrorMessage = "O número deve ter 50 caracteres")]
         public string CliNumero { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Telefone Celular é obrigatório")]
         [StringLength(11, ErrorMessage = "O número de Telefone celular deve ter 11 caracteres")]
         public string CliTelefoneCelular { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Telefone Fixo é obrigatório")]
         [StringLength(10, ErrorMessage = "O número de Telefone fixo deve ter 10 caracteres")]
         public string CliTelefoneFixo { get; set; }
     }
